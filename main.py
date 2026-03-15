a9b25090a4454a5baf35dcda42c5eaedb60066b8
bhojenderIITM <bhojender@bhojenders-MacBook-Air.local>
2026-03-15 11:04:02 +0530

    dvc tracK

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import numpy as np
 import pandas as pd
 train = pd.read_csv("/Users/bhojender/Desktop/IITM/ml-project/DVC/train.csv")
 print(train.isnull().sum())
+print(train.columns)
+train['post_id_edition'] = train['post_id']+10
+print(train.columns)
